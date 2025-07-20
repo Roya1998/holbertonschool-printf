2c61fdc26d8098f1634281cdefe8152a7b87e6b8
Roya1998 <royayusifbayli@gmail.com>
2025-07-20 17:56:04 +0000

    printf file create

--- printf.c
+++ printf.c
@@ -3,47 +3,71 @@
 #include <unistd.h>
 
 /**
- * _printf - Custom printf function
- * @format: Format string
- * Return: Number of characters printed
+ * _printf - produces output according to a format
+ * @format: character string with format specifiers
+ *
+ * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
 	int i = 0, count = 0;
 	va_list args;
-	char *str;
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
+
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			if (!format[i])
-				return (-1);
+				return (-1); /* Handles _printf("%") */
+
 			if (format[i] == 'c')
 			{
 				char c = va_arg(args, int);
-				write(1, &c, 1), count++;
+
+				write(1, &c, 1);
+				count++;
 			}
 			else if (format[i] == 's')
 			{
-				str = va_arg(args, char *);
+				char *str = va_arg(args, char *);
+				int j = 0;
+
 				if (!str)
 					str = "(null)";
-				for (; *str; str++, count++)
-					write(1, str, 1);
+
+				while (str[j])
+				{
+					write(1, &str[j], 1);
+					j++;
+				}
+				count += j;
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
 			}
 			else
-				write(1, format[i] == '%' ? "%" : (char[]){'%', format[i], 0}, format[i] == '%' ? 1 : 2), count += format[i] == '%' ? 1 : 2;
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
 		}
 		else
-			write(1, &format[i], 1), count++;
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
 		i++;
 	}
+
 	va_end(args);
 	return (count);
 }
